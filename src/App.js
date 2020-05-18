914f426757526982edcd374c8c8a1d44372c7895
FarrenRE <farren.esman@gmail.com>
2020-05-18 19:20:06 +1000

    cleanup, add dialogue as Chat prop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './styles/App.scss';
 import Book from './components/Book.js';
 import Chat from './components/Chat.js';
 
+import { dialogue as chapterOneChat } from './dialogue/karellen-1.js';
+
 function App() {
-  const [count, setCount] = useState(0);
   const [chatDisplay, setChatDisplay] = useState(false);
   return (
     <div className="App">
       <Book />
-      <button onClick={ () => { chatDisplay ? setChatDisplay(false) : setChatDisplay(true) } }>Toggle chat</button>
-      {chatDisplay ? <Chat /> : ""}
+      <button style={{width: "100%", height: "3em"}} onClick={ () => { chatDisplay ? setChatDisplay(false) : setChatDisplay(true) } }>Toggle chat</button>
+      {chatDisplay ? <Chat dialogue={chapterOneChat} /> : ""}
     </div>
   );
 }
