76a852999db4822e915bc79365cbb14322f65a09
FarrenRE <farren.esman@gmail.com>
2020-05-18 19:19:21 +1000

    add cover and back cover

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,6 +3,8 @@ import '../styles/Book.scss';
 import '../styles/Page.scss';
 import FlipPage from 'react-flip-page';
 
+import {left as coverLeft} from './pages/cover.js';
+import {right as coverRight} from './pages/cover.js';
 import {front as page1Front} from './pages/1.js';
 import {back as page1Back} from './pages/1.js';
 import {front as page2Front} from './pages/2.js';
@@ -11,12 +13,6 @@ import {front as page3Front} from './pages/3.js';
 import {back as page3Back} from './pages/3.js';
 
 export default function Book() {
-  // const pages = [
-  //   {
-  //     left: page1Front,
-  //     right: page1Back
-  //   }
-  // ];
   return (
     <div className="Book">
         <FlipPage
@@ -24,7 +20,19 @@ export default function Book() {
           uncutPages="true"
           responsive="true"
           perspective="300em"
+          maxAngle="0"
         >
+          <div className="spread cover">
+            <div className="leaf leaf--left cover">
+              {coverLeft()}
+              <span className="page-number"></span>
+            </div>
+            <div className="leaf leaf--right cover">
+              {coverRight()}
+              <span className="page-number"></span>
+            </div>
+          </div>
+          
           <div className="spread">
             <div className="leaf leaf--left">
               {page1Front()}
@@ -35,6 +43,7 @@ export default function Book() {
               <span className="page-number">2</span>
             </div>
           </div>
+
           <div className="spread">
             <div className="leaf leaf--left">
               {page2Front()}
@@ -45,6 +54,7 @@ export default function Book() {
               <span className="page-number">4</span>
             </div>
           </div>
+
           <div className="spread">
             <div className="leaf leaf--left">
               {page3Front()}
@@ -53,6 +63,17 @@ export default function Book() {
             <div className="leaf leaf--right">
               {page3Back()}
               <span className="page-number">6</span>
+            </div>
+          </div>
+
+          <div className="spread back-cover">
+            <div className="leaf leaf--left cover">
+              <h1>The end.</h1>
+              <span className="page-number"></span>
+            </div>
+            <div className="leaf leaf--right cover">
+              
+              <span className="page-number"></span>
             </div>
           </div>
         </FlipPage>
