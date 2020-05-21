78751438cd524040d2e26d42acfbef0773bb79cb
FarrenRE <farren.esman@gmail.com>
2020-05-21 18:28:55 +1000

    remove chat test button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './styles/App.scss';
 import Book from './components/Book.js';
-import Chat from './components/Chat.js';
-
-import { dialogue as chapterOneChat } from './dialogue/karellen-1.js';
 
 function App() {
-  const [chatDisplay, setChatDisplay] = useState(false);
   return (
     <div className="App">
       <Book />
-      <button style={{width: "100%", height: "3em"}} onClick={ () => { chatDisplay ? setChatDisplay(false) : setChatDisplay(true) } }>Toggle chat</button>
-      {chatDisplay ? <Chat dialogue={chapterOneChat} /> : ""}
     </div>
   );
 }
